9bafed673a636d039ed0af9e1c2e217fe538745b
amine3radi <amine3login@gmail.com>
2025-11-03 23:17:14 +0100

    Update trust-badges.js

--- trust-badges.js
+++ trust-badges.js
@@ -1,24 +1,6 @@
-// ===========================================
-// TRUST BADGES & PAYMENT ICONS
-// Adds payment/security badges above buy button
-// ===========================================
-
-(function() {
-    'use strict';
-    
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', init);
-    } else {
-        init();
-    }
-    
-    function init() {
-        setTimeout(addTrustBadges, 100);
-    }
-    
-    function addTrustBadges() {
+function initTrustBadges() {
+    setTimeout(() => {
         const addToCartSection = document.querySelector('.add-to-cart-section');
-        
         if (addToCartSection && !document.getElementById('trust-badges-container')) {
             const badgeImages = [
                 'https://raw.githubusercontent.com/amine3radi/card-badges-cod/70225cac4926414edb57c995873b61d5a2956756/payment.svg',
@@ -26,18 +8,14 @@
                 'https://www.svgrepo.com/show/352446/shipping-fast.svg',
                 'https://raw.githubusercontent.com/amine3radi/card-badges-cod/ccce0c0f04f9692926a932d3e3a75f64c35eb19e/cash-on-delivery.svg'
             ];
-            
-            const container = document.createElement('div');
-            container.id = 'trust-badges-container';
-            
+            const newBadgesContainer = document.createElement('div');
+            newBadgesContainer.id = 'trust-badges-container';
+            let badgesHTML = '';
             badgeImages.forEach(url => {
-                const img = document.createElement('img');
-                img.src = url;
-                img.alt = 'Payment Method';
-                container.appendChild(img);
+                badgesHTML += `<img src="${url}" alt="Trust Badge">`;
             });
-            
-            addToCartSection.parentNode.insertBefore(container, addToCartSection.nextSibling);
+            newBadgesContainer.innerHTML = badgesHTML;
+            addToCartSection.parentNode.insertBefore(newBadgesContainer, addToCartSection.nextSibling);
         }
-    }
-})();
+    }, 500);
+}
